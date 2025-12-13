e91a6ff69140b567c094daa053221175c99f725e
rajkawale <rajendrakawale101@gmail.com>
2025-12-13 22:21:32 +0530

    Add Vercel adapter for server-rendered API routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import partytown from '@astrojs/partytown';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rajkawale.com',
+  output: 'hybrid',
+  adapter: vercel(),
   integrations: [
     tailwind(),
     react(),
